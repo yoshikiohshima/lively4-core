c5dc745f785e24e1a6ca46156a89cd0a5b22b989
null <null>
2018-05-29 12:35:27 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -120,6 +120,7 @@ export default class IpythonTerminal extends Morph {
     var that = this;
  
     this.Services.Kernel.connectTo(this.model, this.settings).then((c) => {
+      debugger;
       that.kernel = c;
       console.log("kernel found") 
       console.log(this.input.value);
