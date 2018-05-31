dab571ad96655868755465ec1af8085f0f0e4d15
null <null>
2018-05-31 10:00:41 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -158,7 +158,7 @@ export default class IpythonTerminal extends Morph {
 
     openNotebook(id) {
         this.notebook = new Notebook(this.token, this);
-        this.notebook.open(id);
+        this.notebook.open(this.sessions[id]);;
     }
   
     sessionSelected(id) {
