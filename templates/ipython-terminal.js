133865e4eea9bebf58b37edfe1a284dcbfb50b63
null <null>
2018-05-30 18:23:09 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -3,221 +3,240 @@
 import Morph from 'src/components/widgets/lively-morph.js';
 
 function iPythonSettings(token) {
-  var Request = window.Services.ServerConnection.defaultSettings.Request;
-  var Headers = window.Services.ServerConnection.defaultSettings.Headers;
-  var WebSocket = window.Services.ServerConnection.defaultSettings.WebSocket;
-  // var fetch = window.Services.ServerConnection.defaultSettings.fetch;
-  var fetch = window.originalFetch;
-  return {baseUrl: 'http://localhost:8888', pageUrl:"", wsUrl: "ws://localhost:8888", token: token,
-               init: {cache: 'no-store', credentials: "same-origin"},
-               Request: Request, Headers: Headers, WebSocket: WebSocket, fetch: fetch};
-    
+    var Request = window.Services.ServerConnection.defaultSettings.Request;
+    var Headers = window.Services.ServerConnection.defaultSettings.Headers;
+    var WebSocket = window.Services.ServerConnection.defaultSettings.WebSocket;
+    // var fetch = window.Services.ServerConnection.defaultSettings.fetch;
+    var fetch = window.originalFetch;
+    return {baseUrl: 'http://localhost:8888', pageUrl:"", wsUrl: "ws://localhost:8888", token: token,
+            init: {cache: 'no-store', credentials: "same-origin"},
+            Request: Request, Headers: Headers, WebSocket: WebSocket, fetch: fetch};
 }
 
 export class Notebook {
-  initialize(token) {
-    this.token = token;
-    this.cells = null;
-    this.model = null;
-  }
+    initialize(token, terminal) {
+	this.token = token;
+	this.terminal = terminal;
+	this.cells = null;
+	this.kernel = null;
+	this.model;
+    }
   
-  async newUntitled() {
-    var settings = iPythonSettings(this.token);
-    var contents = new window.Services.ContentsManager({serverSettings: settings});
-    var result = await contents.newUntitled({path: '.', type: 'notebook', ext: 'ipynb'});
-    this.model = result;
-    this.cells = [];
-    return result;
-  }
+    async newUntitled() {
+	var settings = iPythonSettings(this.token);
+	var contents = new window.Services.ContentsManager({serverSettings: settings});
+	var result = await contents.newUntitled({path: '.', type: 'notebook', ext: 'ipynb'});
+	this.model = result;
+	this.cells = [];
+	this.kernel = this.model.kernel;
+	return result;
+    }
 
-  evaluate(code) {
-      console.log('python evaluate', code);
-      var future = this.kernel.requestExecute({code: code});
-      future.onReply = (reply) => {
-        console.log("execution reply", reply);
-      };
-      future.onIOPub = (reply) => {
-        var type = reply.msg_type;
-        if (type === "status") {
-          if (reply.content.execution_state === "busy") {
-            console.log("kernel started working");
-          } else if (reply.content.execution_state == "idle") {
-            this.addInput();
-          } else {
-          }
-        } else if (type === "execute_input") {
-        } else if (type === "execute_result") {
-          if (reply.content.data && reply.content.data['text/plain'] !== undefined) {
-            this.addOutput(reply.content.data['text/plain']);
-          }
-        } else if (type === "stream") {
-          console.log(reply.content.name, reply.content.text);
-          this.addOutput(reply.content.text);
-        } else if (type === "error") {
-         this.addOutput(reply.content.evalue);
-        }
-      };
-      return future.done;
-  }
+    async open(sessionModel) {
+	var settings = iPythonSettings(this.token);
+	window.Services.Session.connectTo(sessionModel, settings);
+	var contents = new window.Services.ContentsManager({serverSettings: settings});
+	var result = await contents.newUntitled({path: '.', type: 'notebook', ext: 'ipynb'});
+	this.model = result;
+	this.cells = [];
+	this.kernel = this.model.kernel;
+	return result;
+    }
+
+    evaluate(code) {
+	console.log('python evaluate', code);
+	var future = this.kernel.requestExecute({code: code});
+	future.onReply = (reply) => {
+            console.log("execution reply", reply);
+	};
+	future.onIOPub = (reply) => {
+            var type = reply.msg_type;
+            if (type === "status") {
+		if (reply.content.execution_state === "busy") {
+		    console.log("kernel started working");
+		} else if (reply.content.execution_state == "idle") {
+		    this.terminal.addInput();
+		} else {
+		}
+            } else if (type === "execute_input") {
+            } else if (type === "execute_result") {
+		if (reply.content.data && reply.content.data['text/plain'] !== undefined) {
+		    this.terminal.addOutput(reply.content.data['text/plain']);
+		}
+            } else if (type === "stream") {
+		console.log(reply.content.name, reply.content.text);
+		this.terminal.addOutput(reply.content.text);
+            } else if (type === "error") {
+		this.terminal.addOutput(reply.content.evalue);
+            }
+	};
+	return future.done;
+    }
 }
 
 export default class IpythonTerminal extends Morph {
-  initialize() {
-    this.inputs = [];
-    this.outputs = [];
-    this.input = null;
-    this.windowTitle = "IpythonTerminal";
-    this.terminal = this.get("#terminal");
-    this.get('#token').value = 'edfe4b7bc3aa7cc79a14864247378b1eb52b5e8fbb1139b4';
-    this.setupTokenField();
-    this.setupChoices();
-    this.addInput();
+    initialize() {
+	this.inputs = [];
+	this.outputs = [];
+	this.input = null;
+	this.windowTitle = "IpythonTerminal";
+	this.terminal = this.get("#terminal");
+	this.get('#token').value = 'edfe4b7bc3aa7cc79a14864247378b1eb52b5e8fbb1139b4';
 
-    this.kernels = {}; // {name: id}
-  }
-  
-  setSettings() {
-    var Request = this.Services.ServerConnection.defaultSettings.Request;
-    var Headers = this.Services.ServerConnection.defaultSettings.Headers;
-    var WebSocket = this.Services.ServerConnection.defaultSettings.WebSocket;
-    var fetch = this.Services.ServerConnection.defaultSettings.fetch;
-    this.settings = {baseUrl: 'http://localhost:8888', pageUrl:"", wsUrl: "ws://localhost:8888", token: this.token,
-               init: {cache: 'no-store', credentials: "same-origin"},
-               Request: Request, Headers: Headers, WebSocket: WebSocket, fetch: fetch};
-    
-  }
-  
-  kernelSelected(id) {
-    this.modelId = id;
-    this.model = {id: id, name: 'python3'};
-    if (this.kernel) {
-        this.kernel.shutdown();
-        this.kernel == null;
+	this.setupTokenField();
+	this.setupChoices();
+	this.addInput();
+
+	this.sessions = null;
+	this.session = null;
     }
-   }
 
-  setupTokenField() {
-    var field = this.get('#token');
-    field.addEventListener("keyup", (event) => {
-        if (event.keyCode === 13) {
-          this.token = field.value;
-          this.setSettings();
-          this.updateChoices();
-        }
-      });
-     field.addEventListener("click", () => {
-       field.focus();
-     });
-    this.token = field.value;
-  }
-  
-  setupChoices() {
-    var choices = this.get('#modelChoice');
-
-    choices.addEventListener("change", (evt) => {
-        var name = evt.target.value;
-        this.kernelSelected(this.kernels[name]);
-      });
-  }
-
-  updateChoices() {
-    var choices = this.get('#modelChoice');
-    while (choices.options.length > 0) {
-        choices.remove(0);
+    setupTokenField() {
+	var field = this.get('#token');
+	field.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13) {
+		this.token = field.value;
+		this.listSessions(this.token);
+		this.updateChoices();
+            }
+	});
+	field.addEventListener("click", () => {
+	    field.focus();
+	});
+	this.token = field.value;
     }
-    this.kernels = {};
 
-    this.Services.Session.listRunning(this.settings).then((models) => {
-      var firstId = null;
-      for (var i = 0; i < models.length; i++) {
-        var model = models[i];
-        var kernelModel = model.kernel;
-        var id = kernelModel.id;
-        if (!firstId) {
-          firstId = id;
-        }
-        var name = model.name;
-        var option = document.createElement("option");
-        option.text = name;
-        option.modelId = id;
-        choices.add(option);
-     }
-     if( firstId) {
-       this.kernelSelected(firstId);
-     }
+    updateChoices() {
+	var choices = this.get('#modelChoice');
+	while (choices.options.length > 0) {
+            choices.remove(0);
+	}
 
-      console.log('models', models);
-    });   
-  }
+	var firstId = null;
+	for (var i = 0; i < this.sessions.length; i++) {
+	    var sessionModel = this.sessions[i];
+	    var kernelModel = sessionModel.kernel;
+	    var id = sessionModel.id;
+	    if (!firstId) {
+		firstId = id;
+	    }
+	    var name = sessionModel.name;
+	    var option = document.createElement("option");
+	    option.text = name;
+	    option.modelId = id;
+	    choices.add(option);
+	}
+	if(firstId) {
+	    this.kernelSelected(firstId);
+	}
+    }
   
-  addInput() {
-    var text = document.createElement("input");
-    text.classList.add('terminalIn');
-    text.setAttribute("type", "text");
-    this.terminal.appendChild(text);
-    if (this.input) {this.input.blur();}
-    this.input = text;
-    text.addEventListener("keyup", (event) => {
-        if (event.keyCode === 13 && event.shiftKey) {
-          this.runCommand(text);
-        }
-      });
-     this.terminal.addEventListener("click", (event) => {
-       text.focus();
-     });
-    this.input.focus();
-   }
+    async listSessions(token) {
+	var sessions = await window.Services.Session.listRunning(iPythonSettings(token));
+	this.sessions = sessions;
+	this.updateChoices();
 
-  addOutput(str) {
-    var text = document.createElement("div");
-    text.classList.add('terminalOut');
-    text.innerHTML = this.escape(str);
-    this.terminal.appendChild(text);
-  }
+	this.sessions = {};
+	for (var i = 0; i < sessions.length; i++) {
+	    this.sessions[sessions[i].id] = sessions[i];
+	}
+    }
 
-  oneStep(str) {
-    this.addOutput(str);
-    this.addInput();
-  }
+    newNotebook() {
+	this.notebook = new Notebook(this.token, this);
+	this.notebook.newUntitled();
+    }
 
-  escape(str) {
-    return str
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
-   }
+    openNotebook(id) {
+	this.notebook = new Notebook(this.token, this);
+	this.notebook.open(id);
+    }
+  
+    sessionSelected(id) {
+	if (this.session) {
+            this.session.shutdown().then(() => {console.log("session closed")});
+            this.session == null;
+	}
+	this.session = this.sessions[id];
+    }
 
-  runCommand2(text) {
-      console.log('runCommand2', this.input.value);
-      var future = this.kernel.requestExecute({code: text.value});
-      future.onReply = (reply) => {
-        console.log("execution reply", reply);
-      };
-      future.onIOPub = (reply) => {
-        var type = reply.msg_type;
-        if (type === "status") {
-          if (reply.content.execution_state === "busy") {
-            console.log("kernel started working");
-          } else if (reply.content.execution_state == "idle") {
-            this.addInput();
-          } else {
-          }
-        } else if (type === "execute_input") {
-        } else if (type === "execute_result") {
-          if (reply.content.data && reply.content.data['text/plain'] !== undefined) {
-            this.addOutput(reply.content.data['text/plain']);
-          }
-        } else if (type === "stream") {
-          console.log(reply.content.name, reply.content.text);
-          this.addOutput(reply.content.text);
-        } else if (type === "error") {
-         this.addOutput(reply.content.evalue);
-        }
-      };
-      return future.done;
-  }
+    setupChoices() {
+	var choices = this.get('#modelChoice');
+
+	choices.addEventListener("change", (evt) => {
+            var name = evt.target.value;
+            this.kernelSelected(this.kernels[name]);
+	});
+    }
+
+    addInput() {
+	var text = document.createElement("input");
+	text.classList.add('terminalIn');
+	text.setAttribute("type", "text");
+	this.terminal.appendChild(text);
+	if (this.input) {this.input.blur();}
+	this.input = text;
+	text.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13 && event.shiftKey) {
+		this.runCommand(text);
+            }
+	});
+	this.terminal.addEventListener("click", (event) => {
+	    text.focus();
+	});
+	this.input.focus();
+    }
+
+    addOutput(str) {
+	var text = document.createElement("div");
+	text.classList.add('terminalOut');
+	text.innerHTML = this.escape(str);
+	this.terminal.appendChild(text);
+    }
+
+    oneStep(str) {
+	this.addOutput(str);
+	this.addInput();
+    }
+
+    escape(str) {
+	return str
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
+    runCommand2(text) {
+	console.log('runCommand2', this.input.value);
+	var future = this.kernel.requestExecute({code: text.value});
+	future.onReply = (reply) => {
+            console.log("execution reply", reply);
+	};
+	future.onIOPub = (reply) => {
+            var type = reply.msg_type;
+            if (type === "status") {
+		if (reply.content.execution_state === "busy") {
+		    console.log("kernel started working");
+		} else if (reply.content.execution_state == "idle") {
+		    this.addInput();
+		} else {
+		}
+            } else if (type === "execute_input") {
+            } else if (type === "execute_result") {
+		if (reply.content.data && reply.content.data['text/plain'] !== undefined) {
+		    this.addOutput(reply.content.data['text/plain']);
+		}
+            } else if (type === "stream") {
+		console.log(reply.content.name, reply.content.text);
+		this.addOutput(reply.content.text);
+            } else if (type === "error") {
+		this.addOutput(reply.content.evalue);
+            }
+	};
+	return future.done;
+    }
 
   runCommand(text) {
     var that = this;
