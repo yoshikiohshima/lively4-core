c891da361b54ebff70fa1f2af198cf607f9d437c
null <null>
2018-06-04 16:36:14 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -291,7 +291,6 @@ export default class IpythonTerminal extends Morph {
         var cell = cells[i];
         switch (cell.cell_type) {
           case "code":
-            debugger;
             this.addInput(cell.source);
             for (var j = 0; j < cell.outputs.length; j++) {
               var output = cell.outputs[j];
