b29280424e5e82f615d775118df96459b1522a84
null <null>
2018-06-04 13:44:30 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -117,7 +117,7 @@ import numpy as np
 
 def sendTensor():
   comm = Comm(target_name='weights')
-  comm.send(data='test', buffers=[memoryview(np.zeros((2, 2)))])
+  comm.send(data='test', buffers=[memoryview(np.zeros((2, 2), dtype='float32'))])
   comm.close()
 `;
      this.evaluate(code);
