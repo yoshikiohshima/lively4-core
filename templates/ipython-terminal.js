e0b15e8b547afd7ac70c547c65f4a727c5d0e506
null <null>
2018-06-05 13:59:29 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -159,8 +159,6 @@ class Notebook {
    var kernel = this.kernel;
     if (!kernel) {return;}
           kernel.connectToComm("mycomm").then(comm => {
-        console.log("comm", comm);
-      comm.open('ack');
             comm.send("hey");
       comm.onMsg = (msg) => {
         console.log(msg);
