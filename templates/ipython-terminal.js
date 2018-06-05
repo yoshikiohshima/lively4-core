438f33b30bcfed71da00374eaa694eeb6ee1bcda
null <null>
2018-06-04 18:21:51 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -98,17 +98,24 @@ class Notebook {
           }
     }
   
+ 
   setupComm() {
     var kernel = this.kernel;
     if (!kernel) {return;}
-
+    
+    function parseTuple(str) {
+    return eval(str.replace('(', '[').replace(')', ']'));
+  }
     kernel.registerCommTarget('weights', (comm, commMsg) => {
-    comm.onMsg = (msg) => {
-      debugger;
-      console.log(msg);  // 'hello'
-      console.log(msg.buffers[0].buffer);
-      var ar = new Float32Array(msg.buffers[0].buffer);
-      console.log(ar);
+      comm.onMsg = (msg) => {
+      
+       if (msg.content.data === 'dense') {
+         var weights = new Float32Array(msg.buffers[0].buffer);
+         var weightsShape = parseTuple(new TextDecoder('ascii').decode(new Uint8Array(msg.buffers[1].buffer)))
+          var bias = new Float32Array(msg.buffers[2].buffer);
+         var biasShape = parseTuple(new TextDecoder('ascii').decode(new Uint8Array(msg.buffers[3].buffer)))
+       }
+      this.launchWeightsView(weights, weightsShape, bias, biasShape);
     };
     comm.onClose = (msg) => {
       console.log(msg);  // 'bye'
