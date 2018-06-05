b38acdc657e7c764806e87246fff1f1eed06c39b
null <null>
2018-06-05 10:32:21 -0700

    AUTO-COMMIT templates/ipython-terminal.js

--- templates/ipython-terminal.js
+++ templates/ipython-terminal.js
@@ -344,6 +344,7 @@ export default class IpythonTerminal extends Morph {
         if (that.input) {that.input.blur();}
         that.input = text;
         text.addEventListener("keyup", (event) => {
+          debugger;
             if (event.keyCode === 13 && event.shiftKey) {
                 that.runCommand(text);
               return true;
