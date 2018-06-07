29cdb088859d8b9a837265c7a3a0311f1abcacd6
null <null>
2018-06-06 17:23:28 -0700

    AUTO-COMMIT templates/ipython-painter.js

--- templates/ipython-painter.js
+++ templates/ipython-painter.js
@@ -87,8 +87,9 @@ get_ipython().kernel.comm_manager.register_target("mnist_image", handle_open)
     }
 
     var shape = new Uint32Array([28, 28]);
-    terminal.send('mnist_image', 'mnist_image', null, [shape, grayData]);
-    terminal.insertAndRunCommand("mnist_image.evaluate_last_image(evaluator)");
+    terminal.send('mnist_image', 'mnist_image', null, [shape, grayData], ()=> {
+      terminal.insertAndRunCommand("mnist_image.evaluate_last_image(evaluator)");
+    });
   }
   
  initScaledCanvas() {
