e83ff824de917052429f2bf6419c7b5112e4e4c9
null <null>
2018-06-08 10:05:26 -0700

    AUTO-COMMIT templates/lively-list.js

--- templates/lively-list.js
+++ templates/lively-list.js
@@ -5,10 +5,10 @@ import Morph from 'src/components/widgets/lively-morph.js';
 export default class LivelyList extends Morph {
   async initialize() {
     this.windowTitle = "LivelyList";
+    
   }
 
   async livelyExample() {
   }
-  
   
 }
