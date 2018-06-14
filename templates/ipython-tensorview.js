0af821c33ae3f019b5a38a5ed40fd9698f2c08fa
null <null>
2018-06-13 17:07:28 -0700

    AUTO-COMMIT templates/ipython-tensorview.js

--- templates/ipython-tensorview.js
+++ templates/ipython-tensorview.js
@@ -228,7 +228,7 @@ get_ipython().kernel.comm_manager.register_target("weight_tensor", handle_open)
     var tip = window.tooptip;
     console.log(tip);
     if (!tip) {
-      tip = this.get('#tip');
+      tip = window.get('#tip');
     }
     if (!tip) {
       return;
