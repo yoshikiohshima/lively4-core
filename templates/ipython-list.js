b569f79d2a6a22a8b93b75bdaecf074bd46669d6
null <null>
2018-06-18 16:43:32 -0700

    AUTO-COMMIT templates/ipython-list.js

--- templates/ipython-list.js
+++ templates/ipython-list.js
@@ -106,6 +106,25 @@ def set_evaluator(ev):
   global my_evaluator
   my_evaluator = ev
 
+def send_layer_names(ev):
+  if ev is None:
+    evaluator = my_evaluator
+  else:
+    evaluator = ev
+  if evaluator is None:
+    return
+  names = "\n".join(evaluator.get_layer_names())
+  comm = Comm(target_name="layer_names")
+  comm.send(data="dense", buffers=[memoryview(bytearray(names), "ascii")])
+  comm.close()
+
+def receive_layer_names_request(msg):
+  send_layer_names(None)
+
+def handle_open(comm, msg):
+  comm.on_msg(receive_layer_names_request)
+
+get_ipython().kernel.comm_manager.register_target("layer_names", handle_open)
 ''')`
     terminal.runCommand(py);
 /*    terminal.insertAndRunCommand('layer_names.set_evaluator(evaluator)');*/    terminal.addHandler('layer_names', this, this.receive_layer_names.bind(this));
