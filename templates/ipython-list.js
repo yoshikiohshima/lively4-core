d2aa5de08a7f8bd2dda8e2adb253d634d4891297
null <null>
2018-06-18 16:48:04 -0700

    AUTO-COMMIT templates/ipython-list.js

--- templates/ipython-list.js
+++ templates/ipython-list.js
@@ -113,7 +113,10 @@ def send_layer_names(ev):
     evaluator = ev
   if evaluator is None:
     return
-  names = "\n".join(evaluator.get_layer_names())
+  names = "aa"
+  comm = Comm(target_name="layer_names")
+  comm.send(data="dense", buffers=[memoryview(bytearray(names), "ascii")])
+  comm.close()
 
 def receive_layer_names_request(msg):
   send_layer_names(None)
