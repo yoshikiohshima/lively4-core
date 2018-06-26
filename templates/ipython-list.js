b13daafdad0bb8cad5a82da75201d64fad034885
null <null>
2018-06-25 18:25:56 -0700

    AUTO-COMMIT templates/ipython-list.js

--- templates/ipython-list.js
+++ templates/ipython-list.js
@@ -135,7 +135,7 @@ def handle_open(comm, msg):
 get_ipython().kernel.comm_manager.register_target("layer_names", handle_open)
 ''')`;
 
-    terminal.addHandler('layer_names', this, this.receive_layer_names.bind(this));
+    terminal.addHandler('layer_names', this.id, this.receive_layer_names.bind(this));
 
     terminal.runCommand(py);
     terminal.insertAndRunCommand(`
